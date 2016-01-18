dcc50bfe72acb14f17320e2ce55e3f97a9af2648
David LeGare <excaliburhissheath@gmail.com>
2016-01-18 07:40:42 -0600

    feature(component): add singleton component with update
    Add a variant of `SingletonComponentManager` that handles components that
    impl `ComponentUpdate`. This is a temporary fix until specialization drops
    which will allow us to roll this functionality back into
    `SingletonComponentManager`.

--- src/component/singleton_component_manager.rs
+++ src/component/singleton_component_manager.rs
@@ -73,3 +73,80 @@ impl<T, U> ComponentManager for SingletonComponentManager<T>
 
     fn destroy(&self, _: Entity) {}
 }
+
+// =======================================
+// SINGLETON COMPONENT MANAGER WITH UPDATE
+// =======================================
+
+// TODO: Having a separate type for this won't be necessary once specialization is implemented.
+
+#[derive(Debug, Clone)]
+pub struct SingletonComponentUpdateManager<T>
+    where T: Component<Manager=SingletonComponentUpdateManager<T>> + ComponentUpdate + Debug + Clone + Default,
+          T::Message: Message<Target=T>,
+{
+    data: T,
+    messages: RefCell<Vec<T::Message>>,
+}
+
+impl<T> SingletonComponentUpdateManager<T>
+    where T: Component<Manager=SingletonComponentUpdateManager<T>> + ComponentUpdate + Debug + Clone + Default,
+          T::Message: Message<Target=T>,
+{
+    pub fn new(data: T) -> SingletonComponentUpdateManager<T> {
+        SingletonComponentUpdateManager {
+            data: data,
+            messages: RefCell::new(Vec::new()),
+        }
+    }
+
+    pub fn send_message<U: Into<T::Message>>(&self, message: U) {
+        self.messages.borrow_mut().push(message.into());
+    }
+}
+
+impl<T> Deref for SingletonComponentUpdateManager<T>
+    where T: Component<Manager=SingletonComponentUpdateManager<T>> + ComponentUpdate + Debug + Clone + Default,
+          T::Message: Message<Target=T>,
+{
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.data
+    }
+}
+
+impl<T> DerefMut for SingletonComponentUpdateManager<T>
+    where T: Component<Manager=SingletonComponentUpdateManager<T>> + ComponentUpdate + Debug + Clone + Default,
+          T::Message: Message<Target=T>,
+{
+    fn deref_mut(&mut self) -> &mut T {
+        &mut self.data
+    }
+}
+
+impl<T, U> ComponentManagerBase for SingletonComponentUpdateManager<T>
+    where T: Component<Manager=SingletonComponentUpdateManager<T>, Message=U> + ComponentUpdate + Debug + Clone + Default,
+          U: Message<Target=T>,
+{
+    fn update(&mut self) {
+        let mut messages = self.messages.borrow_mut();
+        for message in messages.drain(..) {
+            message.apply(&mut self.data);
+        }
+        self.data.update();
+    }
+}
+
+impl<T, U> ComponentManager for SingletonComponentUpdateManager<T>
+    where T: Component<Manager=SingletonComponentUpdateManager<T>, Message=U> + ComponentUpdate + Debug + Clone + Default,
+          U: Message<Target=T>,
+{
+    type Component = T;
+
+    fn register(builder: &mut EngineBuilder) {
+        builder.register_manager(SingletonComponentUpdateManager::new(T::default()));
+    }
+
+    fn destroy(&self, _: Entity) {}
+}
