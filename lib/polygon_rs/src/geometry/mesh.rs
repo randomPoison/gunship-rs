f83514cddf379d7e72b7200408073fa19afe70e5
David LeGare <excaliburhissheath@gmail.com>
2016-01-30 17:53:15 -0600

    Rework MeshBuilder To Make Setting UVs Easier
    This change reduces support for multiple UVs per vertex but texture
    support didn't exist anyway so whatever.

--- lib/polygon_rs/src/geometry/mesh.rs
+++ lib/polygon_rs/src/geometry/mesh.rs
@@ -77,15 +77,6 @@ pub enum BuildMeshError {
     IncorrectAttributeCount,
 }
 
-#[derive(Debug, Clone)]
-pub struct MeshBuilder {
-    position_data: Vec<Point>,
-    normal_data: Vec<Vector3>,
-    texcoord_data: Vec<Vec<Vector2>>,
-
-    indices:  Vec<u32>,
-}
-
 /// Provides a safe interface for building a mesh from raw vertex data.
 ///
 /// `MeshBuilder` supports two methods for specifying mesh data: Providing a series of `Vertex`
@@ -98,6 +89,15 @@ pub struct MeshBuilder {
 /// - Check for different data count for different attributes (e.g. if the position attribute data
 ///   for a different number of elements than the normal attribute).
 /// - Any of the indicies would be out of bounds for the given vertex data.
+#[derive(Debug, Clone)]
+pub struct MeshBuilder {
+    position_data: Vec<Point>,
+    normal_data: Vec<Vector3>,
+    texcoord_data: Vec<Vector2>,
+
+    indices:  Vec<u32>,
+}
+
 impl MeshBuilder {
     pub fn new() -> MeshBuilder {
         MeshBuilder {
@@ -115,15 +115,11 @@ impl MeshBuilder {
             self.normal_data.push(normal);
         }
 
-        // Ensure there is a list for each texcoord.
-        while self.texcoord_data.len() < vertex.texcoord.len() {
-            self.texcoord_data.push(Vec::new());
-        }
+        assert!(vertex.texcoord.len() <= 1, "More than one texcoord per vertex is currently not supported");
 
         // Add each texcoord to its corresponding list.
-        let iter = vertex.texcoord.iter().zip(self.texcoord_data.iter_mut());
-        for (texcoord, texcoord_data) in iter {
-            texcoord_data.push(*texcoord);
+        if vertex.texcoord.len() > 0 {
+            self.texcoord_data.push(vertex.texcoord[0])
         }
     }
 
@@ -144,16 +140,9 @@ impl MeshBuilder {
         self
     }
 
-    pub fn set_texcoord_data(mut self, texcoord_data: &[&[Vector2]]) -> MeshBuilder {
-        if texcoord_data.len() == 0 {
-            return self;
-        }
-
-        for (src, dest) in texcoord_data.iter().zip(self.texcoord_data.iter_mut()) {
-            dest.clear();
-            dest.extend(*src);
-        }
-
+    pub fn set_texcoord_data(mut self, texcoord_data: &[Vector2]) -> MeshBuilder {
+        self.texcoord_data.clear();
+        self.texcoord_data.extend(texcoord_data);
         self
     }
 
@@ -173,10 +162,8 @@ impl MeshBuilder {
             return Err(BuildMeshError::IncorrectAttributeCount);
         }
 
-        for texcoord_set in &self.texcoord_data {
-            if texcoord_set.len() != vertex_count {
-                return Err(BuildMeshError::IncorrectAttributeCount);
-            }
+        if self.texcoord_data.len() != vertex_count {
+            return Err(BuildMeshError::IncorrectAttributeCount);
         }
 
         // Make sure all indices at least point to a valid vertex.
@@ -191,16 +178,10 @@ impl MeshBuilder {
 
         // TODO: Check for degenerate triangles? Actually, should that be a failure or a warning?
 
-        let float_count = {
-            let mut float_count =
-                self.position_data.len() * 4
-              + self.normal_data.len() * 3;
-            for texcoord_set in &self.texcoord_data {
-                float_count += texcoord_set.len() * 2;
-            }
-
-            float_count
-        };
+        let float_count =
+            self.position_data.len() * 4
+          + self.normal_data.len() * 3
+          + self.texcoord_data.len() * 2;
 
         // Create the mesh.
         let mut vertex_data = Vec::<f32>::with_capacity(float_count);
@@ -227,12 +208,12 @@ impl MeshBuilder {
 
         // Setup texcoord data.
         let mut texcoord_attribs = Vec::new();
-        for texcoord_set in &self.texcoord_data {
+        {
             texcoord_attribs.push(VertexAttribute {
                 offset: vertex_data.len(),
                 stride: 2,
             });
-            vertex_data.extend(Vector2::as_ref(&*texcoord_set));
+            vertex_data.extend(Vector2::as_ref(&*self.texcoord_data));
         }
 
         // By our powers combined! We are! A mesh.
