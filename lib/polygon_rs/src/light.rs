acd30313b75a7cac517f71d6464d2631f9675daf
David LeGare <excaliburhissheath@gmail.com>
2016-01-31 11:37:56 -0600

    Default Constructor For Point Lights
    Add `Light::point()` to default construct a point light.

--- lib/polygon_rs/src/light.rs
+++ lib/polygon_rs/src/light.rs
@@ -5,6 +5,14 @@ pub enum Light {
     Point(PointLight),
 }
 
+impl Light {
+    pub fn point() -> Light {
+        Light::Point(PointLight {
+            position: Point::origin(),
+        })
+    }
+}
+
 #[derive(Clone, Copy, Debug)]
 pub struct PointLight {
     pub position: Point,
