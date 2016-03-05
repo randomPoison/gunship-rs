2ac4ef0cea0794eed9d9cda036aa4ccea2d6c4f6
David LeGare <excaliburhissheath@gmail.com>
2016-03-04 21:30:30 -0600

    Use Example Mesh For Wireframe Example
    Change wireframe example for gl-util to use parse-obj to load an example
    mesh and render it as a wireframe.

--- lib/gl-util/examples/wireframe.rs
+++ lib/gl-util/examples/wireframe.rs
@@ -1,31 +1,30 @@
 extern crate bootstrap_rs as bootstrap;
 extern crate gl_util as gl;
+extern crate parse_obj;
 
 use bootstrap::window::*;
 use gl::*;
-
-static VERTEX_POSITIONS: [f32; 9] = [
-    -1.0, -1.0, 0.0,
-     1.0, -1.0, 0.0,
-     0.0,  1.0, 0.0,
-];
-
-static VERTEX_INDICES: [u32; 6] = [
-    0, 1,
-    1, 2,
-    2, 0,
-];
+use parse_obj::*;
 
 fn main() {
     let mut window = Window::new("gl-util - wireframe example");
 
+    // Load mesh file and normalize indices for OpenGL.
+    let obj = Obj::from_file("examples/epps_head.obj").unwrap();
+    let mut raw_indices = Vec::new();
+    for face in obj.faces() {
+        for index in face {
+            raw_indices.push(*index as u32 - 1);
+        }
+    }
+
     gl::init();
     let mut vertex_buffer = VertexBuffer::new();
-    vertex_buffer.set_data_f32(&VERTEX_POSITIONS[..]);
-    vertex_buffer.set_attrib_f32(AttributeLocation::from_index(0), 3, 0, 0);
+    vertex_buffer.set_data_f32(obj.raw_positions());
+    vertex_buffer.set_attrib_f32(AttributeLocation::from_index(0), 4, 0, 0);
 
     let mut index_buffer = IndexBuffer::new();
-    index_buffer.set_data_u32(&VERTEX_INDICES[..]);
+    index_buffer.set_data_u32(&*raw_indices);
 
     'outer: loop {
         while let Some(message) = window.next_message() {
@@ -35,7 +34,7 @@ fn main() {
             }
 
             gl::clear();
-            vertex_buffer.draw_elements(DrawMode::Lines, &index_buffer);
+            vertex_buffer.draw_wireframe(DrawMode::Triangles, &index_buffer);
             gl::swap_buffers();
         }
     }
