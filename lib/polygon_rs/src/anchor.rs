82b036fcfc52b6d1b885ef5edfa7dcf0ca965c55
David LeGare <excaliburhissheath@gmail.com>
2016-04-12 20:33:42 -0500

    refactor(polygon::anchor): add getters and setters for position, orientation, and scale

--- lib/polygon_rs/src/anchor.rs
+++ lib/polygon_rs/src/anchor.rs
@@ -23,11 +23,42 @@ impl Anchor {
         }
     }
 
+    /// Gets the current position of the anchor.
+    pub fn position(&self) -> Point {
+        self.position
+    }
+
+    /// Sets the position of the anchor.
+    pub fn set_position(&mut self, position: Point) {
+        self.position = position;
+    }
+
+    /// Gets the current orientation of the anchor.
+    pub fn orientation(&self) -> Quaternion {
+        self.orientation
+    }
+
+    /// Sets the orientation of the anchor.
+    pub fn set_orientation(&mut self, orientation: Quaternion) {
+        self.orientation = orientation;
+    }
+
+    /// Gets the current scale of the anchor.
+    pub fn scale(&self) -> Vector3 {
+        self.scale
+    }
+
+    /// Sets the scale of the anchor.
+    pub fn set_scale(&mut self, scale: Vector3) {
+        self.scale = scale;
+    }
+
     /// Attaches a mesh to the anchor, allowing the mesh to be rendered in the scene.
     pub fn attach_mesh(&mut self, gpu_mesh: GpuMesh) {
         self.meshes.insert(gpu_mesh);
     }
 
+    /// Gets the set of meshes attached to the anchor.
     pub fn meshes(&self) -> &HashSet<GpuMesh> {
         &self.meshes
     }
